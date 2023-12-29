42dedc40081a4980a014fac1a255122c212aede6
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-12-29 14:46:57 -0300

    Add tailwindcss

---
+++ .storybook/preview.ts
@@ -0,0 +1,29 @@
+import type { Preview } from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+
+import "./styles.css";
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "dark",
+    attributeName: "data-mode",
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
