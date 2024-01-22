d5487ef60429be6a97174fbfd486605b2499793a
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-01-22 18:08:20 -0300

    Fix input field to control styles by its own and not its container

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -14,39 +14,45 @@ export const InputField = forwardRef(function ForwardedInputField(
   ref: Ref<HTMLInputElement>,
 ) {
   return (
-    <div
-      className={classNames(
-        className,
-        "group flex gap-2 min-w-48 justify-stretch",
-        "bg-white text-gray-900 border border-gray-300 placeholder:text-gray-500",
-        "shadow-sm rounded-lg py-2.5 px-3.5 text-base h-11 outline-none",
-        "focus-within:border-primary-300 focus-within:ring-4 focus-within:ring-primary-100",
-        "has-[:disabled]:bg-gray-50 has-[:disabled]:text-gray-500 has-[:disabled]:cursor-not-allowed",
-        "has-[:invalid]:border-error-300",
-        "has-[:invalid]:focus-within:border-error-300 has-[:invalid]:focus-within:ring-error-100",
-      )}
-    >
-      {leadingIcon !== undefined && (
-        <div className="flex shrink-0 items-center justify-center h-full min-w-5 text-gray-500">
-          {leadingIcon}
-        </div>
-      )}
-
+    <div className={classNames(className, "group relative")}>
       <input
         {...props}
         ref={ref}
         size={1}
         className={classNames(
-          "outline-none appearance-none flex w-full bg-transparent",
-          "disabled:bg-inherit disabled:text-inherit disabled:cursor-[inherit]",
+          "outline-none appearance-none flex min-w-48 w-full",
+          "bg-white text-gray-900 border border-gray-300 placeholder:text-gray-500",
+          "shadow-sm rounded-lg py-2.5 pr-[2.625rem] text-base h-11",
+          "focus:border-primary-300 focus:ring-4 focus:ring-primary-100",
+          "disabled:bg-gray-50 disabled:text-gray-500 disabled:cursor-not-allowed",
+          "invalid:border-error-300",
+          "invalid:focus:border-error-300 invalid:focus:ring-error-100",
+          {
+            "pl-[2.625rem]": leadingIcon !== undefined,
+            "pl-3.5": leadingIcon === undefined,
+          },
         )}
         onBlur={handleBlur}
       />
 
+      {leadingIcon !== undefined && (
+        <div
+          className={classNames(
+            "absolute top-0 bottom-0 left-3.5",
+            "flex shrink-0 items-center justify-center w-5 text-gray-500",
+            "pointer-events-none",
+          )}
+        >
+          {leadingIcon}
+        </div>
+      )}
+
       {helpIcon && (
         <div
           className={classNames(
-            "flex shrink-0 items-center justify-center w-4 text-gray-400",
+            "absolute top-0 bottom-0 right-3.5",
+            "flex shrink-0 items-center justify-center w-5 text-gray-500",
+            "pointer-events-none",
             "group-has-[:invalid]:text-error-500",
           )}
         >
