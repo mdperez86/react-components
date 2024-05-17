16e8ff524963debd5b02df49aa9d462ec14b2e80
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-17 11:42:30 -0400

    Upgrading storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../**/*.stories.@(ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -11,17 +12,22 @@ const config: StorybookConfig = {
     "@storybook/addon-themes",
     "@storybook/addon-a11y",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   async viteFinal(config) {
     return mergeConfig(config, {
       plugins: [tsconfigPaths()],
     });
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
