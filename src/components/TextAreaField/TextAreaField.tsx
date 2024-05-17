d36a9717114954391768d12c1900bae064ad0a99
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-17 13:55:09 -0400

    Fix textarea field padding and leading icon alignment

--- src/components/TextAreaField/TextAreaField.tsx
+++ src/components/TextAreaField/TextAreaField.tsx
@@ -17,36 +17,51 @@ export const TextAreaField = forwardRef(function ForwardedTextAreaField(
     <div
       className={classNames(
         className,
-        "group flex gap-2 min-w-48 justify-stretch",
-        "bg-white text-gray-900 border border-gray-300 placeholder:text-gray-500",
-        "shadow-sm rounded-lg py-2.5 px-3.5 text-base outline-none",
+        "group relative overflow-hidden",
+        "shadow-sm rounded-lg",
+        "border border-gray-300",
         "focus-within:border-primary-300 focus-within:ring-4 focus-within:ring-primary-100",
-        "has-[:disabled]:bg-gray-50 has-[:disabled]:text-gray-500 has-[:disabled]:cursor-not-allowed",
         "has-[:invalid]:border-error-300",
         "has-[:invalid]:focus-within:border-error-300 has-[:invalid]:focus-within:ring-error-100",
       )}
     >
-      {leadingIcon !== undefined && (
-        <div className="flex shrink-0 items-center justify-center h-5 min-w-5 text-gray-500">
-          {leadingIcon}
-        </div>
-      )}
-
       <textarea
         rows={4}
         {...props}
         ref={ref}
         className={classNames(
-          "outline-none appearance-none flex w-full bg-transparent",
-          "disabled:bg-inherit disabled:text-inherit disabled:cursor-[inherit]",
+          "outline-none appearance-none flex min-w-48 min-h-11 w-full",
+          "bg-white text-gray-900 placeholder:text-gray-500",
+          "disabled:bg-gray-50 disabled:text-gray-500 disabled:cursor-not-allowed",
+          "py-2.5 text-base",
+          {
+            "pl-[2.625rem]": leadingIcon !== undefined,
+            "pl-3.5": leadingIcon === undefined,
+            "pr-[2.625rem]": helpIcon,
+            "pr-3.5": !helpIcon,
+          },
         )}
         onBlur={handleBlur}
       />
 
+      {leadingIcon !== undefined && (
+        <div
+          className={classNames(
+            "absolute top-0 h-11 left-3.5",
+            "flex shrink-0 items-center justify-center w-5 text-gray-500",
+            "pointer-events-none",
+          )}
+        >
+          {leadingIcon}
+        </div>
+      )}
+
       {helpIcon && (
         <div
           className={classNames(
-            "flex shrink-0 items-center justify-center w-4 text-gray-400",
+            "absolute top-0 bottom-0 right-3.5",
+            "flex shrink-0 items-center justify-center w-5 text-gray-500",
+            "pointer-events-none",
             "group-has-[:invalid]:text-error-500",
           )}
         >
