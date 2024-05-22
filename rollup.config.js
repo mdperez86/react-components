af827a9cab2bfa593e1e6df1f60d34169c705717
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-22 12:16:19 -0400

    Remove dts plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { dts } from "rollup-plugin-dts";
-
-//NEW
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
@@ -52,14 +49,4 @@ export default [
       terser(),
     ],
   },
-  // {
-  //   input: "dist/esm/types/src/index.d.ts",
-  //   output: [{ file: packageJson.types, format: "esm" }],
-  //   plugins: [dts({ tsconfig: "./tsconfig.rollup-esm.json" })],
-  // },
-  // {
-  //   input: "dist/cjs/types/tailwind.config.d.ts",
-  //   output: [{ file: "dist/tailwind.config.d.ts", format: "esm" }],
-  //   plugins: [dts({ tsconfig: "./tsconfig.rollup-cjs.json" })],
-  // },
 ];
