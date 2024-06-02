1d89c4a95d85e4446b272d73e29ed663a87ae60f
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-06-01 20:01:10 -0400

    Add the badge prop to the NavMenuItem component

--- src/components/NavMenu/NavMenuItem/NavMenuItem.tsx
+++ src/components/NavMenu/NavMenuItem/NavMenuItem.tsx
@@ -1,9 +1,10 @@
 import { type Ref, forwardRef } from "react";
 import classNames from "classnames";
+import { Badge } from "@this/components/Badge";
 import type { NavMenuItemProps } from "./types";
 
 export const NavMenuItem = forwardRef(function ForwardedNavMenuItem(
-  { text, className, ...props }: NavMenuItemProps,
+  { text, badge, className, ...props }: NavMenuItemProps,
   ref: Ref<HTMLLIElement>,
 ) {
   return (
@@ -11,9 +12,20 @@ export const NavMenuItem = forwardRef(function ForwardedNavMenuItem(
       <a
         href="#"
         role="menuitem"
-        className={classNames("p-3 rounded-lg hover:bg-gray-50")}
+        className={classNames("flex p-3 rounded-lg hover:bg-gray-50")}
       >
-        <span className="text-md font-medium text-gray-900">{text}</span>
+        <div className="flex items-center gap-2">
+          <span className="text-md font-medium text-gray-900">{text}</span>
+
+          {badge && (
+            <Badge
+              size="xs"
+              className="text-md font-medium text-success-700 bg-success-50"
+            >
+              {badge}
+            </Badge>
+          )}
+        </div>
       </a>
     </li>
   );
--- src/components/NavMenu/NavMenuItem/types.ts
+++ src/components/NavMenu/NavMenuItem/types.ts
@@ -2,5 +2,12 @@ export type NavMenuItemProps = React.DetailedHTMLProps<
   React.LiHTMLAttributes<HTMLLIElement>,
   HTMLLIElement
 > & {
+  /**
+   * The item text.
+   */
   text: string;
+  /**
+   * The item badge.
+   */
+  badge?: string;
 };
